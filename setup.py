ea7dfa01279e48cb9e0c628034d41711d62afdff
ipickering (lab) <ign.pickering@gmail.com>
2019-06-03 15:22:51 -0400

    Fix setup

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 import sys
 #print(find_packages())
+with open('README.md','r') as descfile:
+    long_description = descfile.read()
 setup_attrs = {
         'name': 'pychmech',
         'version':'0.1.0',
         'description':'Simple molecular dynamics simulators for gases',
+        'long_description':long_description,
+        'long_desctiption_content_type':'text/markdown',
         'url':'https://github.com/IgnacioJPickering/cm_simulator/pycmech',
         'author':'Ignacio Pickering',
         'author_email':'ign.pickering@gmail.com',
@@ -13,3 +17,4 @@ setup_attrs = {
         'include_package_data':True,
         'install_requires':['numpy','matplotlib']
         }
+setup(**setup_attrs)
