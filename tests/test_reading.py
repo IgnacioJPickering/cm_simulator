166c5766098470e6a2a26a2e5cb1dbd8b3a2a182
ipickering (lab) <ign.pickering@gmail.com>
2019-06-03 17:06:22 -0400

    Fix test

--- tests/test_reading.py
+++ tests/test_reading.py
@@ -1,29 +1,39 @@
 import sys
 import numpy as np
 import pycmech as cm
-#from ..pycmech import pycmech
+
+water_path = './test_data/water.xyz'
 input_path = './test_data/some_atoms.xyz'
 input_pathv = './test_data/some_atoms_with_velocs.xyz'
 input_pathvc = './test_data/some_atoms_with_velocs_corrupted.xyz'
-chem_list = ['X','H','He','Li','Be','B','C','N','O','F','Ne','Na',
-        'Mg','Al','Si','P','S','Cl','Ar','K','Ca','Sc','Ti','V',
-        'Cr','Mn','Fe','Co','Ni','Cu','Zn','Ga','Ge','As','Se','Br',
-        'Kr','Rb','Sr','Y','Zr','Nb','Mo','Tc','Ru','Rh','Pd','Ag',
-        'Cd','In','Sn','Sb','Te','I','Xe','Cs','Ba','La','Ce','Pr','Nd',
-        'Pm','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu',
-        'Hf','Ta','W','Re','Os','Ir','Pt','Au','Hg','Tl','Pb','Bi','Po',
-        'At','Rn','Fr','Ra','Ac','Th','Pa','U','Np','Pu']
-mass_list = [0.,1.00811,4.002602,6.997,9.0121831,10.821,12.0116,
-        14.00728,15.99977,18.998403,20.1797]
-mass_dict = {znum:mass for znum,mass in enumerate(mass_list)}
-chem_dict = {key:value for value,key in enumerate(chem_list)}
-coordinates, znumbers = cm.read_coordinates_from_xyz(input_path,chem_dict)
-symbols = cm.znumbers_to_symbols(znumbers,chem_dict)
-masses = cm.get_atomic_masses(znumbers,mass_dict)
+def test_coord_water():
+    coordinates, znumbers = cm.read_coordinates_from_xyz(water_path)
+    assert np.allclose(coordinates, [[ 1.88972612,  0.0,  0.0 ], 
+        [ 1.88972612,  1.88972612,  9.44863062], 
+        [ 0.0,  3.77945225, 13.22808287]],rtol=1e-08,atol=1e-10)
+
+def test_znum_water():
+    coordinates, znumbers = cm.read_coordinates_from_xyz(water_path)
+    assert np.array_equal(znumbers,[8,1,1])
+
+
+def test_symbols():
+    symbols = cm.znumbers_to_symbols([8,1,1])
+    assert np.array_equal(symbols,['O','H','H'])
+
+
+def test_masses():
+    masses = cm.get_atomic_masses([8,1,1])
+    assert np.allclose(masses,[15.99977,1.00811,1.00811],rtol=1e-10,atol=1e-12)
+
+
+coordinates, znumbers = cm.read_coordinates_from_xyz(input_path)
+symbols = cm.znumbers_to_symbols(znumbers)
+masses = cm.get_atomic_masses(znumbers)
 velocities = cm.generate_velocities(coordinates,'uniform',10)
 particles = cm.ParticleGroup(coordinates,velocities,masses,znumbers)
 particles = cm.ParticleGroup.from_xyz(input_path,('uniform',10))
-particles = cm.ParticleGroup.from_xyz(input_pathv,distribution=False)
+particles = cm.ParticleGroup.from_xyz(input_pathv,velocities='xyz')
 particles = cm.ParticleGroup.from_xyz(input_path,('constant',0))
 #particles = cm.ParticleGroup.from_xyz(input_pathvc,distribution=False)
 print(particles.initial_coordinates)
